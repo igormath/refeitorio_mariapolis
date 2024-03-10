3da7064a79c753f125d3e29258b9a0b534740656
igormath <igorxt9@gmail.com>
2024-03-09 21:07:11 -0300

    Adiciona classes de validacao

---
+++ src/main/java/br/upe/validation/ValidaFuncionario.java
@@ -0,0 +1,33 @@
+package br.upe.validation;
+
+import br.upe.entities.Employee;
+import br.upe.util.Facade;
+
+import java.util.List;
+
+public class ValidaFuncionario {
+    public ValidaFuncionario() {
+    }
+
+    public static boolean isValido(String cpf, String senha){
+        if (!ValidaCpf.isCPF(cpf)){
+            return false;
+        }
+
+        if (senha.length() < 6){
+            return false;
+        }
+
+        Facade facade = new Facade();
+        List<Employee> employees = facade.getAllEmployees();
+
+        for (Employee employee : employees) {
+            if (cpf.equals(employee.getCpf())){
+                if (senha.equals(employee.getPass())){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
