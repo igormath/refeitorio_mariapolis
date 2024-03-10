58ba52fcf8b50cced61d22a4bc5a7a2390ec832f
igormath <igorxt9@gmail.com>
2024-03-09 21:12:33 -0300

    Adiciona componente de mascara p/ fxml e classe de mudanca de cena

---
+++ src/main/java/br/upe/util/SceneChanger.java
@@ -0,0 +1,29 @@
+package br.upe.util;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
+public class SceneChanger {
+    private static Scene scene;
+
+    public static void setScene(Scene scene) {
+        SceneChanger.scene = scene;
+    }
+    public static void setInitialScene(Scene initialScene) {
+        scene = initialScene;
+    }
+
+    public static void changeScene(String fxml) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(SceneChanger.class.getResource("/br/upe/fxml/" + fxml + ".fxml"));
+            Parent root = fxmlLoader.load();
+            scene.setRoot(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
