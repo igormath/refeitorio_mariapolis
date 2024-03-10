45bdae19d1b638cf541b7dcd7c0a67e721c7b09a
igormath <igorxt9@gmail.com>
2024-03-09 21:11:21 -0300

    Adiciona classe Util de conexao com o BD

--- src/main/java/br/upe/util/JPAUtil.java
+++ src/main/java/br/upe/util/JPAUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JPAUtil {
-    public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+    public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("centro_mariapolis");
 
     public static EntityManager getEntityManager(){
         return emf.createEntityManager();
